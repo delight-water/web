a1d3e31cd847195bf482c8b16da799bea29e5367
maheshkcot95 <maheshkcot95@gmail.com>
2021-03-10 15:16:31 +0530

    Add Animation

---
+++ assets/js/custom.js
@@ -0,0 +1,20 @@
+// equal heights
+var maxHeight = 0;
+
+$("div.oe1").each(function(){
+    if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
+});
+
+$("div.oe1").height(maxHeight);
+
+
+
+
+/*  water ripple */
+
+$(".rippleContent").ripples({
+    resolution: 1024,
+    dropRadius: 20,
+    interactive: true,
+    perturbance: 0.02,
+});
